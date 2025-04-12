57c2d5b11d309fe937670b8ea423f1ba7ffa6c96
Syed Safdar Ali Shah <programmersafdar@live.com>
2025-04-12 09:19:50 +0500

    Testing_fastApi

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+@app.get("/items/")
+def read_items(q: str = None):
+    return {"q": q}
+@app.get("/items/{item_id}/details")
+def read_item_details(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
